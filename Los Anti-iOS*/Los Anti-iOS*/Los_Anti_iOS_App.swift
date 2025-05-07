a61ebab20adc9300ca56256886ff1995e446957f
Leyenddx <aarok_ny224@hotmail.com>
2025-05-07 10:20:04 -0600

    [CREAMOS] El proyecto de iOS

---
+++ Los Anti-iOS*/Los Anti-iOS*/Los_Anti_iOS_App.swift
@@ -0,0 +1,17 @@
+//
+//  Los_Anti_iOS_App.swift
+//  Los Anti-iOS*
+//
+//  Created by alumno on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Los_Anti_iOS_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
